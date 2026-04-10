cb5a0b2c2df8ab0ea9224b4ced052d7b425be9c7
Thu Rain Hein <thurainhein097@gmail.com>
2026-04-10 11:30:51 +0630

    third

--- lib/src/trh_template_runner.dart
+++ lib/src/trh_template_runner.dart
@@ -5,7 +5,7 @@ import 'package:args/args.dart';
 
 import 'template_contents.dart';
 
-const _currentCliVersion = '0.1.0';
+const _currentCliVersion = '0.1.1';
 const _repoUrl = 'https://github.com/thurain11/trh_flutter_template.git';
 const _ansiReset = '\x1B[0m';
 const _ansiYellow = '\x1B[33m';
